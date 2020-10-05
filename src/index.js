dd2b5f533592756058ee6e3143df8a67e786003e
Ramantsevich Dmitriy <d.ramantsevich@gmail.com>
2020-10-06 00:15:54 +0300

    feat: implicit towelSort function

--- src/index.js
+++ src/index.js
@@ -2,5 +2,24 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+    let emptyArray = [];
+
+    if(matrix == undefined){
+        return emptyArray;
+    }else if(matrix.length == 0){
+        return emptyArray;
+    }else if(matrix.length > 3){
+        let secondArray = matrix[1].reverse();
+        let newArray = matrix.fill(secondArray, 1, 1);
+        let thirdArray = matrix[3].reverse();
+        newArray = matrix.fill(thirdArray, 3);
+
+        return newArray.flat(Infinity);
+    }
+    else{
+        let secondArray = matrix[1].reverse();
+        let newArray = matrix.fill(secondArray, 1, 1);
+
+        return newArray.flat(Infinity);
+    }
 }
